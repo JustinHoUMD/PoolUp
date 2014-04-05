35beb5dc8c5aabcb552b62c1161dfdfa1d94556d
JustinHoUMD <itsjustinn@gmail.com>
2014-04-04 23:37:21 -0400

    Added input fields to get username and password

--- PoolUp/src/com/mobile/poolup/MainActivity.java
+++ PoolUp/src/com/mobile/poolup/MainActivity.java
@@ -3,13 +3,33 @@ package com.mobile.poolup;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
+	EditText username; 
+	EditText password; 
+	Button login; 
+	String u,p;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        username = (EditText)findViewById(R.id.usernameText);
+        password = (EditText)findViewById(R.id.passwordText); 
+        login = (Button)findViewById(R.id.loginButton);
+        login.setOnClickListener(
+                new View.OnClickListener()
+                {
+                    public void onClick(View view)
+                    {
+                    	 u = username.getText().toString(); 
+                         p = password.getText().toString(); 
+                    }
+                });
+       
     }
 
 
