c5c2746d09fcc697574ef8a74027c21554846cb8
JustinHoUMD <itsjustinn@gmail.com>
2014-04-06 01:04:48 -0400

    List passengers in trip info

--- PoolUp/src/com/mobile/poolup/TripInfoActivity.java
+++ PoolUp/src/com/mobile/poolup/TripInfoActivity.java
@@ -33,6 +33,7 @@ public class TripInfoActivity extends Activity{
 		tripCode = bundle.getString("TRIP_CODE");
 		Log.d("DEBUG", "Received code: " + tripCode);		
 		getTripDetails();
+		getPassengers(); 
 		//displayDetails();
 		
 		
@@ -63,6 +64,34 @@ public class TripInfoActivity extends Activity{
 		        for(String k : tripCodes.keySet()){	
 		        	TextView tv = new TextView(getApplicationContext());
 		        	String s = tripCodes.get(k).toString();
+		        	tv.setText(k+ ":"+s);		        	
+					lm.addView(tv);
+		        }		        
+		     
+		    }
+			@Override
+			public void onCancelled(FirebaseError arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+		   
+		});
+	}
+	
+	private void getPassengers(){
+		Firebase currentUser = fireBaseRef.child("trips").child(tripCode).child("Passengers");		
+		currentUser.addValueEventListener(new ValueEventListener() {
+		    @Override
+		    public void onDataChange(DataSnapshot snapshot) {
+		    	GenericTypeIndicator<Map<String, Object>> t = new GenericTypeIndicator<Map<String, Object>>() {};
+		        Map<String, Object> tripCodes = snapshot.getValue(t);       
+		        
+		        
+		        LinearLayout lm = (LinearLayout) findViewById(R.id.lTripLayout);
+		        for(String k : tripCodes.keySet()){	
+		        	TextView tv = new TextView(getApplicationContext());
+		        	String s = k.toString(); 
 		        	tv.setText(s);		        	
 					lm.addView(tv);
 		        }		        
@@ -77,6 +106,7 @@ public class TripInfoActivity extends Activity{
 		   
 		});
 	}
+	
 	
 	
 
